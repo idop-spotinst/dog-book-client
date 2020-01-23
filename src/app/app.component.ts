33dd61e713b32d9d87c2bf135a2b0cc19e7a7fcc
idop <ido.postelnik@spotinst.com>
2020-01-23 13:14:28 +0200

    [src] Set sample of create dog form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dog-book-client';
+
+  name: string;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateDog(dog: {name: string, userId: number}) {
+    let retVal = {
+      name: this.name,
+      // userId: 1
+    };
+    console.log('retVal: ', retVal);
+    this.http.post('http://localhost:3000/api/dog', retVal)
+    .subscribe(responseData => {
+      console.log('responseData: ', responseData);
+    });
+  }
 }
